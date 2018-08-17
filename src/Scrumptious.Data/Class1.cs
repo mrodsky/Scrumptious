2c0a1d08f86e63d9def1c0e71cb7bfff18060c10
Vincent Tso <v.tso@outlook.com>
2018-08-17 11:04:06 -0400

    setup p2

---
+++ src/Scrumptious.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Scrumptious.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Scrumptious.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Scrumptious.Library
+{
+    public class Class1
+    {
+    }
+}
