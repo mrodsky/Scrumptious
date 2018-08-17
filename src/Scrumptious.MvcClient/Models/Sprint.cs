1e69d19718739a0105344f435cd0ecb72390b16b
Vincent Tso <v.tso@outlook.com>
2018-08-17 11:06:51 -0400

    initialize db-first

---
+++ src/Scrumptious.MvcClient/Models/Sprint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scrumptious.MvcClient.Models
+{
+    public partial class Sprint
+    {
+        public Sprint()
+        {
+            Backlog = new HashSet<Backlog>();
+        }
+
+        public int SprintId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string SprintDescription { get; set; }
+        public bool Completed { get; set; }
+        public int FkProjectId { get; set; }
+
+        public Project FkProject { get; set; }
+        public ICollection<Backlog> Backlog { get; set; }
+    }
+}
