fd990a1af889a84ce5f8b18de0d54f04bc5f9dd6
ccarrjoh <courtneyjohnson928@gmail.com>
2018-08-20 17:26:33 -0400

    made task tests

---
+++ test/Scrumptious.Testing.Library/TaskTests.cs
@@ -0,0 +1,59 @@
+﻿using Scrumptious.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Scrumptious.Testing.Library
+{
+    public partial class TaskTests
+    {
+        private readonly Task sut = new Task()
+        {
+            Name = "Test",
+            TaskDescription = "Get all these tests working",
+            Requirements = "1. write good code",
+            Step = new List<Step>(),
+            Completed = false
+        };
+
+        [Theory]
+        [InlineData("Test2")]
+        public void Test_ModTaskName(string NewName)
+        {
+            sut.Name = NewName;
+            
+            Assert.Equal("Test2", sut.Name);
+        }
+
+        [Theory]
+        [InlineData("Tests have passed")]
+        public void Test_ModTaskDescription(string NewDescription)
+        {
+            sut.TaskDescription = NewDescription;
+            Assert.Equal("Tests have passed", sut.TaskDescription);
+        }
+
+        [Theory]
+        [InlineData("1. write good code 2. running tests 3. Pass tests")]
+        public void Test_ModTaskRequirements(string NewRequirements)
+        {
+            sut.Requirements = NewRequirements;
+
+            Assert.Equal("1. write good code 2. running tests 3. Pass tests", sut.Requirements);
+        }
+
+        [Fact]
+        public void Test_AddStep()
+        {
+            var a = new Step();
+
+            sut.Step.Add(a);
+            Assert.True(sut.Step.Count == 1);
+        }
+
+    }
+}
