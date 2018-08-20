6eec690ee7d788fc22ef827488a052f03c8069ef
Michael Rodsky <mrodsky@gmail.com>
2018-08-20 11:39:52 -0400

    added ConfigurationBuilder to parse appSetting.json

--- src/Scrumptious.Data/scrumptiousdbContext.cs
+++ src/Scrumptious.Data/scrumptiousdbContext.cs
@@ -27,7 +27,8 @@ namespace Scrumptious.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var options = new ConfigurationBuilder().AddJsonFile("appSetting.dev.json");
+            var options = new ConfigurationBuilder().AddJsonFile("appSetting.dev.json").Build();
+            optionsBuilder.UseSqlServer(options["connectionString"]);
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
