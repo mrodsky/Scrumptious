286ba2eaffe3841704174b82f076aa87e352a739
Michael Rodsky <mrodsky@gmail.com>
2018-08-20 13:57:57 -0400

    accidental changes to master, deleted mvcclient.models, changed testin csproj to xunit

--- test/Scrumptious.Testing.Library/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Scrumptious.Testing.Library
-{
-    public class Class1
-    {
-    }
-}
---
+++ test/Scrumptious.Testing.Library/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Scrumptious.Testing.Library
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
