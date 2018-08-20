ae89e6a354e1d432d868cde89e9c7b610cb18ef9
Vincent Tso <v.tso@outlook.com>
2018-08-20 16:34:30 -0400

    i am screaming ever louder. also, this is half the library.

---
+++ test/Scrumptious.Testing.Library/BacklogTests.cs
@@ -0,0 +1,21 @@
+﻿using Scrumptious.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Scrumptious.Testing.Library
+{
+    public class BacklogTests
+    {
+        private Backlog sut = new Backlog();
+
+        [Fact]
+        public void Test_BacklogAddTask()
+        {
+            sut.AddTask();
+            Assert.True(1 <= sut.Task.Count());
+        }
+    }
+}
