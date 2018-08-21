dda7ad035b4c439c201f908b72ebb26c194350d1
Michael Rodsky <mrodsky@gmail.com>
2018-08-21 09:07:35 -0400

    added generic read and save, mock context and FirstMigration if we need it

---
+++ src/Scrumptious.Data/Models/EntityData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scrumptious.Data.Models
+{
+    public class EntityData
+    {
+        private static MockContext Mock = new MockContext();
+
+        private static scrumptiousdbContext context = new scrumptiousdbContext();
+
+        public EntityData() { }
+
+
+
+        public void Save<T>(T a) where T : class
+        {
+            if (typeof(T) == typeof(Backlog))
+            {
+                context.Backlog.Add(a as Backlog);
+                context.SaveChanges();
+            }
+            else if (typeof(T) == typeof(Project))
+            {
+                context.Project.Add(a as Project);
+                context.SaveChanges();
+            }
+            else if (typeof(T) == typeof(Sprint))
+            {
+
+                context.Sprint.Add(a as Sprint);
+                context.SaveChanges();
+            }
+            else if (typeof(T) == typeof(Step))
+            {
+
+                context.Step.Add(a as Step);
+                context.SaveChanges();
+            }
+            else if (typeof(T) == typeof(Task))
+            {
+
+                context.Task.Add(a as Task);
+                context.SaveChanges();
+            }
+            else if (typeof(T) == typeof(User))
+            {
+
+                context.User.Add(a as User);
+                context.SaveChanges();
+            }
+        }
+            public T Read<T>(int id) where T : class
+            {
+            if (typeof(T) == typeof(Project))
+            {
+                return context.Project.SingleOrDefault(u => u.ProjectId == id) as T;
+            }
+            else if (typeof(T) == typeof(Backlog))
+            {
+                return context.Backlog.SingleOrDefault(u => u.BacklogId == id) as T;
+            }
+            else if (typeof(T) == typeof(Sprint))
+            {
+                return context.Sprint.SingleOrDefault(u => u.SprintId == id) as T;
+            }
+            if (typeof(T) == typeof(Step))
+            {
+                return context.Step.SingleOrDefault(u => u.StepId == id) as T;
+            }
+            else if (typeof(T) == typeof(Task))
+            {
+                return context.Task.SingleOrDefault(u => u.TaskId == id) as T;
+            }
+            else if (typeof(T) == typeof(User))
+            {
+                return context.User.SingleOrDefault(u => u.UserId == id) as T;
+            }
+            else
+                return default(T);
+        }
+    }
+}
