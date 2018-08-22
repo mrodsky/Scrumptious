aceed744ad17ce1a862e1c3080eb8d8bf6fa3e34
kurtispe <kurtispedersen42@gmail.com>
2018-08-22 11:44:58 -0400

    cors set up complete

--- src/Scrumptious.Service/Startup.cs
+++ src/Scrumptious.Service/Startup.cs
@@ -5,16 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Scrumptious.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Scrumptious.Service
 {
     public class Startup
     {
+        public IConfiguration Configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json", optional: true)
+            .Build();
+
+            services.AddDbContext<scrumptiousdbContext>(o => o.UseSqlServer(Configuration["connectionString"]));
+            services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +40,9 @@ namespace Scrumptious.Service
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+
+            app.UseCors(o => o.AllowAnyOrigin().AllowAnyMethod());
+            app.UseMvc();
         }
     }
 }
