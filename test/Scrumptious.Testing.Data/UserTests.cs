34357856046550fd427284d0913d39e68d81780d
Michael Rodsky <mrodsky@gmail.com>
2018-08-22 14:37:57 -0400

    all Testing.Data tests are done all models are tested saving and reading and passing

--- test/Scrumptious.Testing.Data/UserTests.cs
+++ test/Scrumptious.Testing.Data/UserTests.cs
@@ -2,6 +2,7 @@
 using Scrumptious.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -9,11 +10,52 @@ namespace Scrumptious.Testing.Data
 {
    public class UserTests
     {
+        private User sut;
         private readonly MockContext mock = new MockContext();
         private readonly EntityData entity = new EntityData();
         private readonly scrumptiousdbContext ctx = new scrumptiousdbContext();
 
+        public UserTests()
+        {
+            sut = new User()
+            {
+                FirstName = "Barney",
+                LastName = "TheDinosaur",
+                Email = "ILoveYou@Gmail.com",
+                Role = "TheBest",
+                Task = null
+            };
+        }
 
- 
+        [Fact]
+        public void Test_UserId()
+        {
+            User u1 = new User();
+            mock.SaveAsync(sut);
+            mock.SaveAsync(u1);
+            Assert.True(sut.UserId != u1.UserId);
+            Assert.True(typeof(int) == sut.UserId.GetType());
+        }
+
+        [Fact]
+        public void Test_User_Save_Mock()
+        {
+            var expected = 1;
+            mock.SaveAsync<User>(sut);
+            var actual = mock.User.Count();
+
+            Assert.True(expected <= actual);
+
+        }
+
+        [Fact]
+        public void Test_User_Read_Mock()
+        {
+            mock.SaveAsync<User>(sut);
+            var expected = sut.UserId;
+            var actual = mock.ReadList<User>(1);
+            Assert.Equal(expected, actual.UserId);
+        }
+
     }
 }
